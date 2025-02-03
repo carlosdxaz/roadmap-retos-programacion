615aa4adcbf3b2222d692283149e078645bec638
carlosdxaz <carlosdxaz@hotmail.com>
2025-02-03 13:07:06 -0600

    carlosdxaz.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/carlosdxaz.py
@@ -0,0 +1,23 @@
+# https://www.python.org
+
+# ¿Cómo hago un comentario en varios líneas? Usando """ ya sean comillas simples o dobles 
+""" 
+comentario en varias
+líneas 
+""""
+''' 
+también es un comentario 
+'''
+# Cómo crear una variable 
+edad = 21
+nombre = "Carlos"
+# En python no existen constantes, pero puedes hacer esto 
+
+
+#Datos primitivos 
+my_int = 1
+my_float = 1.5
+my_bool = true 
+my_string = "hola"
+
+print ("Hola mundo")
